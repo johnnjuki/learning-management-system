2ae05d9c22c36a59d8f794c24f9890a056225c9c
John Njuki <njukijohn2000@gmail.com>
2023-09-24 14:42:48 +0300

    feat: Add navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,37 @@
-'use client';
+"use client";
 
+import { usePathname, useRouter } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogOut } from "lucide-react";
+
+import { Button } from "./ui/button";
 
 export const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  // Used to change the navbar based on the current page
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter"); // individual course page
+
   return (
-    <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+    <div className="ml-auto flex gap-x-2">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="mr-2 h-4 w-4" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
-}
+  );
+};
