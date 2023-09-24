7ffb27b5c3668334c1322c44e497e0e6936e7bca
John Njuki <njukijohn2000@gmail.com>
2023-09-24 13:59:59 +0300

    feat: Add sidebar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function AuthLayout({ children}: { children: React.ReactNode}) {
   return (
     <div className='h-full flex items-center justify-center'>{children}</div>
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "./_components/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full">
+      <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col md:flex">
+        <Sidebar />
+      </div>
+      <main className="h-full md:pl-56">{children}</main>
+    </div>
+  );
+}
